4fed9aeae6709e79d27e1422b29576461ea8d523
imaki <imaki@imaki.jp>
2020-04-04 18:31:56 +0900

    first_commit

---
+++ src/test/java/com/example/racquet/RacquetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.racquet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RacquetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
